2fbf454b094693436bdb607657c3177e67b07d5e
Mikhail2309 <107263418+Mikhail2309@users.noreply.github.com>
2022-06-10 17:46:26 +0300

    Add files via upload
    111

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int x1 = 1 ;
+    int x2 = 2;
+    int x3 = 3;
+    int x4 = x1 + x2 + x3;
+
+    char c = (char )x;
+    std::cout << c << std::endl;
+    std::cout << x << std::endl;
+    std::cout << x4 << std::endl;
+    return 0;
+}
